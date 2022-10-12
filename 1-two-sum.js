68f6d663339184e150e32e8b492963f729bdbb98
Mustafa Binalhag <mbin828@gmail.com>
2022-10-12 16:41:43 -0400

    two sum speed down to 88ms 19ms faster than the last attempt and less memory -1.3MB

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -5,13 +5,12 @@
  */
 var twoSum = function (nums, target) {
   let instaLookup = {};
-  nums.forEach((element, index) => {
-    instaLookup[element] = index;
-  });
   for (let i = 0; i < nums.length; i++) {
     let desiredNum = target - nums[i];
-    if (instaLookup[desiredNum] && i != instaLookup[desiredNum]) {
+    if (instaLookup[desiredNum] != undefined && i != instaLookup[desiredNum]) {
       return [i, instaLookup[desiredNum]];
+    } else {
+      instaLookup[nums[i]] = i;
     }
   }
 };
