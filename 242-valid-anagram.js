73db3bafc8f0d87f719280dc1b643b47971e1f96
Mustafa Binalhag <mbin828@gmail.com>
2022-10-12 15:14:52 -0400

    much cleaner but doesnt improve performance that much 171ms and 49.1MB

--- 242-valid-anagram.js
+++ 242-valid-anagram.js
@@ -4,20 +4,8 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  if (s.length !== t.length) return false;
-  let sHash = {};
-  let isAnagram = true;
-  for (const letter of s) {
-    sHash[letter] = sHash[letter] + 1 || 1;
-  }
-  let tHash = {};
-  for (const letter of t) {
-    tHash[letter] = tHash[letter] + 1 || 1;
-  }
-  Object.keys(sHash).forEach((key) => {
-    if (sHash[key] !== tHash[key]) {
-      return (isAnagram = false);
-    }
-  });
-  return isAnagram;
+  let arr1 = s.split("").sort();
+  let arr2 = t.split("").sort();
+
+  return arr1.join("") === arr2.join("");
 };
