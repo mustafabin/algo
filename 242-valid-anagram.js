9b552d8286dfdfd1a1583f28efee76e529a56466
Mustafa Binalhag <mbin828@gmail.com>
2022-10-12 15:27:14 -0400

    one liner 0 performance benefits but looks nice

--- 242-valid-anagram.js
+++ 242-valid-anagram.js
@@ -4,8 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  let arr1 = s.split("").sort();
-  let arr2 = t.split("").sort();
-
-  return arr1.join("") === arr2.join("");
+  return s.split("").sort().join("") === t.split("").sort().join("");
 };
