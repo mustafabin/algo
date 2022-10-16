9b55ff58035c318fa7be2defc75fe859883ada22
Mustafa Binalhag <mbin828@gmail.com>
2022-10-16 18:29:50 -0400

    completed medium but with subpar time complexity O(n)+O(n log n) + O(k)

--- medium/347-frequent-elements.js
+++ medium/347-frequent-elements.js
@@ -3,4 +3,26 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function (nums, k) {};
+var topKFrequent = function (nums, k) {
+    let setMap = {}
+    // O(n)
+    for (let i = 0; i < nums.length; i++) {
+        // setMap[nums[i]] = setMap[nums[i]] || 0 + 1
+        if( setMap[nums[i]]){
+            setMap[nums[i]] ++
+        }else{
+            setMap[nums[i]] = 1
+        }
+    }
+    // O(n log n)
+    let sortedSet = Object.entries(setMap).sort((a,b)=>b[1] - a[1])
+    let result = []
+    // O(k)
+    for (let i = 0; i < k; i++) {
+        result.push(sortedSet[i][0])
+    }
+    return result;
+
+};
+
+console.log(topKFrequent([1,1,1,2,2,3], 2)) // expected [1,2]
