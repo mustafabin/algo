386eb54a154a5479470419591d0b28f366783dbe
Mustafa Binalhag <mbin828@gmail.com>
2022-10-18 14:29:13 -0400

    3 lines for a medium

--- medium/347-frequent-elements.js
+++ medium/347-frequent-elements.js
@@ -5,24 +5,8 @@
  */
 var topKFrequent = function (nums, k) {
     let setMap = {}
-    // O(n)
-    for (let i = 0; i < nums.length; i++) {
-        // setMap[nums[i]] = setMap[nums[i]] || 0 + 1
-        if( setMap[nums[i]]){
-            setMap[nums[i]] ++
-        }else{
-            setMap[nums[i]] = 1
-        }
-    }
-    // O(n log n)
-    let sortedSet = Object.entries(setMap).sort((a,b)=>b[1] - a[1])
-    let result = []
-    // O(k)
-    for (let i = 0; i < k; i++) {
-        result.push(sortedSet[i][0])
-    }
-    return result;
-
+    for(let num of nums){ setMap[num] = 1 + (setMap[num] || 0)}
+    return Object.entries(setMap).sort((a,b)=>b[1] - a[1]).slice(0,k).map((item)=>item[0]);
 };
 
-console.log(topKFrequent([1,1,1,2,2,3], 2)) // expected [1,2]
+console.log(topKFrequent([4,1,-1,2,-1,2,3], 2)) // expected [2, -1]
