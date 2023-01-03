409898b7e4030ffcf94f2fbe9dd27c15a38199ed
Mustafa Binalhag <mbin828@gmail.com>
2023-01-03 16:58:43 -0500

    attempt one

---
+++ easy/168-excel-sheet-col-title.js
@@ -0,0 +1,52 @@
+// first attempt 
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+// var convertToTitle = function(columnNumber) {
+//     let dictionary = {
+//         0: "A",
+//         1: "A",
+//         2: "B",
+//         3: "C",
+//         4: "D",
+//         5: "E",
+//         6: "F",
+//         7: "G",
+//         8: "H",
+//         9: "I",
+//         10: "J",
+//         a: "K",
+//         b: "L",
+//         c: "M",
+//         d: "N",
+//         e: "O",
+//         f: "P",
+//         g: "Q",
+//         h: "R",
+//         i: "S",
+//         j: "T",
+//         k: "U",
+//         l: "V",
+//         m: "W",
+//         n: "X",
+//         o: "Y",
+//         p: "Z",
+//     }
+//     let convertedBase = parseInt(columnNumber,10).toString(26)
+//     let result = ""
+//     for(let letter of convertedBase){
+//         result += dictionary[letter]
+//     }
+//     return result
+// };
+
+// second attempt
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+   
+    return result
+};
